8bc082ab5f624bcb6b3d2e4a8e12d2fc20955c13
AubinP <aubin.porte@gmail.com>
2018-07-03 17:25:06 +0200

    Add files via upload

---
+++ randomsplit.py
@@ -0,0 +1,35 @@
+import random, shutil, glob, os
+from os.path import isfile, join
+from os import listdir
+
+# Setup paths variable
+path = "C:\\Users\\Aubin\\Documents\\HCube Conseil\\DeepLearning\\ImagesDatasets"
+target_folder_images = "C:\\Users\\Aubin\\Documents\\HCube Conseil\\DeepLearning\\Sandbox\\"
+target_folder = "C:\\Users\\Aubin\\Documents\\HCube Conseil\\DeepLearning\\Sandbox\\Folder"
+
+# User insert the number he want
+number_pictures_taken = int(input("Enter the number images to extract: "))
+
+# Initializing
+i = 0
+j = 1
+k = 0
+
+
+filename = [f for f in listdir(path) if isfile(join(path, f))]
+for x in range(number_pictures_taken):
+	try:
+		shutil.copyfile(path + '\\' + filename[x], target_folder_images + filename[x]) # Copy file from path to the target folder
+	except:
+		print('Cannot insert path in the array!')
+
+random.shuffle(filename) # Put name in a random index in the array
+
+for i in range(number_pictures_taken):
+	target_folder_temp = target_folder + str(j) + '\\' # Loop the end number of the folder
+	shutil.move(target_folder_images + filename[i], target_folder_temp) # Move images into sub folders
+	# Alternate Folder1 to Folder5 in a loop
+	if j == 5:
+		j = 1
+	else:
+		j = j + 1
